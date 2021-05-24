17d7fe8920690279e5cba634bc790ff9153a846f
akram1214 <akramhosenbhola11@gmail.com>
2021-05-24 12:56:14 +0600

    all code edit

---
+++ Extra Data/all_challenge/Challenge-3/js/script.js
@@ -0,0 +1,14 @@
+//SLICK SLIDER
+
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    autoplay: true,
+    autoplayspeed: 1000,
+    speed: 1000,
+    pauseOnHover: false,
+});
